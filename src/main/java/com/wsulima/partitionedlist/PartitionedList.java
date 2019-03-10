8921edfcfdc2d74cecda45f4369b5440172a1998
Waldemar Sulima <waldemarsulima@gmail.com>
2019-03-10 19:17:56 +0100

    PartitionedList + unit tests implementation.

--- src/main/java/com/wsulima/partitionedlist/PartitionedList.java
+++ src/main/java/com/wsulima/partitionedlist/PartitionedList.java
@@ -1,4 +1,57 @@
 package com.wsulima.partitionedlist;
 
-public class PartitionedList {
+import java.util.AbstractList;
+import java.util.List;
+
+public class PartitionedList<T> extends AbstractList<List<T>> {
+
+    private final List<T> baseList;
+    private final int partitionSize;
+
+    /**
+     * Constructor
+     *
+     * @param list
+     * @param partitionSize
+     */
+    public PartitionedList(List<T> list, int partitionSize) {
+        this.baseList = list;
+        this.partitionSize = partitionSize;
+    }
+
+    /**
+     * Static method giving as a result list of given list's subpartitions
+     *
+     * @param list
+     * @param partitionSize
+     * @return list of subpartitions lists
+     */
+    public static <T> List<List<T>> partition(List<T> list, int partitionSize) {
+        return new PartitionedList(list, partitionSize);
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    @Override
+    public List<T> get(int index) {
+        final int numberOfPartitions = this.size();
+
+        if (index < 0) throw new IndexOutOfBoundsException("Index can't be a negative number");
+        if (index >= numberOfPartitions)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size:" + numberOfPartitions);
+
+        final int start = index * partitionSize;
+        final int end = Math.min(start + partitionSize, baseList.size());
+        return baseList.subList(start, end);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    @Override
+    public int size() {
+        return (baseList.size() + partitionSize - 1) / partitionSize;
+    }
 }
--- src/test/java/com/wsulima/partitionedlist/PartitionedListTests.java
+++ src/test/java/com/wsulima/partitionedlist/PartitionedListTests.java
@@ -1,7 +1,120 @@
 package com.wsulima.partitionedlist;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PartitionedListTests {
+
+    @Test
+    public void oneIntElement() {
+        // GIVEN
+        int initialListElem = 1;
+        List testList = Arrays.asList(initialListElem);
+
+        // WHEN
+        List<List> partitionsList = PartitionedList.partition(testList, 1);
+
+        // THEN
+        assertThat(partitionsList).isNotNull().hasSize(1);
+        assertThat(partitionsList.get(0)).isInstanceOf(List.class);
+
+        List partition = partitionsList.get(0);
+        assertThat(partition).hasSize(1);
+        assertThat(partition.get(0)).isEqualTo(initialListElem);
+    }
+
+    @Test
+    public void oneStringElem() {
+        // GIVEN
+        String initialListElem = "1";
+        List testList = Arrays.asList(initialListElem);
+
+        // WHEN
+        List<List> partitionsList = PartitionedList.partition(testList, 1);
+
+        // THEN
+        assertThat(partitionsList).isNotNull().hasSize(1);
+        assertThat(partitionsList.get(0)).isInstanceOf(List.class);
+
+        List partition = partitionsList.get(0);
+        assertThat(partition).hasSize(1);
+        assertThat(partition.get(0)).isEqualTo(initialListElem);
+    }
+
+    @Test
+    public void fiveElemsTwoSizePartition() {
+        // GIVEN
+        List testList = Arrays.asList("1", "2", "3", "4", "5");
+
+        // WHEN
+        List<List> partitions = PartitionedList.partition(testList, 2);
+
+        // THEN
+        assertThat(partitions).isNotNull().hasSize(3);
+        assertThat(partitions.get(0)).hasSize(2).containsExactly("1", "2");
+        assertThat(partitions.get(1)).hasSize(2).containsExactly("3", "4");
+        assertThat(partitions.get(2)).hasSize(1).containsExactly("5");
+    }
+
+    @Test
+    public void fiveElemsThreeSizePartition() {
+        // GIVEN
+        List testList = Arrays.asList("1", "2", "3", "4", "5");
+
+        // WHEN
+        List<List> partitions = PartitionedList.partition(testList, 3);
+
+        // THEN
+        assertThat(partitions).isNotNull().hasSize(2);
+        assertThat(partitions.get(0)).hasSize(3).containsExactly("1", "2", "3");
+        assertThat(partitions.get(1)).hasSize(2).containsExactly("4", "5");
+    }
+
+    @Test
+    public void fiveElemsOneSizePartition() {
+        // GIVEN
+        List testList = Arrays.asList("1", "2", "3", "4", "5");
+
+        // WHEN
+        List<List> partitions = PartitionedList.partition(testList, 1);
+
+        // THEN
+        assertThat(partitions).isNotNull().hasSize(5);
+        assertThat(partitions.get(0)).hasSize(1).containsExactly("1");
+        assertThat(partitions.get(1)).hasSize(1).containsExactly("2");
+        assertThat(partitions.get(2)).hasSize(1).containsExactly("3");
+        assertThat(partitions.get(3)).hasSize(1).containsExactly("4");
+        assertThat(partitions.get(4)).hasSize(1).containsExactly("5");
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testErrorOnNegativeIndex() {
+        // GIVEN
+        List testList = Arrays.asList("1", "2", "3", "4", "5");
+
+        // WHEN
+        List<List> partitions = PartitionedList.partition(testList, 1);
+
+        partitions.get(-5);
+
+        // THEN throws exception
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testErrorOnExcedingIndex() {
+        // GIVEN
+        List testList = Arrays.asList("1", "2", "3", "4", "5");
+
+        // WHEN
+        List<List> partitions = PartitionedList.partition(testList, 1);
+
+        partitions.get(1001);
+
+        // THEN throws exception
+    }
 
 }
